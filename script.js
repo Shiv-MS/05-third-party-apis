400ca7b2e025bcce7534a65aee9fcfb6d1e8315c
Shivam Shewa <shewashivam@gmail.com>
2020-10-21 19:44:44 -0400

    Fixed Color Scheme

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ $(document).ready(() => {
     for (let i = 1; i < 24; i++) {
         $(".container").append(`<div class="row time-block" id="${i <= 12 ? i : i - 12}AM">
         <div class="col-lg-1 hour ${i < current_hour.getHours() ? "past" : "future"}">${i <= 12 ? `${i}AM` : `${i - 12}PM`}</div>
-        <textarea class="col-lg-10 description"></textarea>
+        <textarea class="col-lg-10 description ${i < current_hour.getHours() ? "past" : "future"}"></textarea>
         <button hour=${i} class="saveBtn col-lg-1"><i class="fas fa-save"></i></button>
       </div>`)
     }
